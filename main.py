b705e7e8ba7c0a2d5c042b9bc02c97aa3c4d0894
7belrhalmia <wbelrhalmia@gmal.com>
2017-10-29 15:40:34 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -19,7 +19,16 @@ def get_tokens(file):
 
 def sup_noise(Str):
         # return re.sub(r'\d+:\d+:\d+,\d* --> \d+:\d+:\d+','',Str)
-        return re.sub(r'\d*','',re.sub(r'\(.+\)','',re.sub(r'\d+:\d+:\d+,\d* --> \d+:\d+:\d+,','',Str)))
+        Str=re.sub(r'\d+:\d+:\d+,\d* --> \d+:\d+:\d+,','',Str)
+        Str=re.sub(r'\(.+\)','',Str)
+        Str=re.sub(r'\[.+\]','',Str)
+        Str=re.sub(r'\d*','',Str)
+        Str=re.sub(r'<i>','',Str)
+        Str=re.sub(r'</i>','',Str)
+        Str=re.sub(r'Sync and correction by GeirDM','',Str)
+        Str=re.sub(r'www\.addiced\.com','',Str)
+            
+        return Str
 
 dir='/home/belr/Projet_TAL/train_data'
 dir_h='/home/belr/Projet_TAL/train_data_postprocessed'
@@ -28,7 +37,6 @@ dict_label={}
 i=0
 # print os.listdir(".")
 for dirnames in os.walk(dir).next()[1]:
-        print(dirnames)
         if not os.path.exists(dir_h+os.path.sep+dirnames):
            os.makedirs(dir_h+os.path.sep+dirnames)
         subf= dir+os.path.sep+dirnames
@@ -40,11 +48,10 @@ for dirnames in os.walk(dir).next()[1]:
             #     s.append(t)
             res= sup_noise(file.read())
             # res=clean_text(res)
-            res = ''.join([c for c in res if c not in ('!','.','?',',',':')])
+            # res = ''.join([c for c in res if c not in ('!','.','?',',',':','#')])
             tokens=nltk.word_tokenize(res)
-            # g=open(dir_h+os.path.sep+dirnames+os.path.sep+fil,"w")
-            # [g.write(x+' ') for x in tokens]
-            print tokens
+            g=open(dir_h+os.path.sep+dirnames+os.path.sep+fil,"w")
+            [g.write(x+' ') for x in tokens]
             # [shutil.rmtree(dir_h+os.path.sep+x) for x in os.walk(dir_h).next()[1]]
 
 
